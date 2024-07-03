d4593c73226094dce843a918fed7ea3057a62941
rytjr <kks007159@naver.com>
2024-07-03 18:59:42 +0900

    heap

---
+++ src/MinHeap/MinHeap.java
@@ -0,0 +1,32 @@
+package MinHeap;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class MinHeap {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int Num = sc.nextInt();
+		
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+		
+		for(int i = 0; i < Num; i++) {
+			
+			int num = sc.nextInt();
+			
+			if(num == 0) {
+				sb.append(que.size() == 0 ? 0 : que.poll()).append("\n");
+			}
+			else {
+				que.add(num);
+			}
+		}
+		
+		System.out.print(sb);
+		
+	}
+
+}
